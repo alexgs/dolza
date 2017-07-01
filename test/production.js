1abdc8f8483f68c9d917d0c47dc1d3ddb9af1f8c
Phil Gates-Shannon <phil.gs@gmail.com>
2017-07-01 06:46:27 -0400

    Add production build script and tests

--- test/production.js
+++ test/production.js
@@ -12,6 +12,9 @@ const expect = chai.expect;
 chai.use( dirtyChai );
 
 describe( 'In production, Dolza', function() {
+    // The production library exports a "Dolza" singleton, and the "require" module system also enforces a singleton
+    // pattern. There is no way to "reset" the DI container before each test; the same container is reused in every
+    // test here. Each test must be mindful of this.
     before( function () {
         _.forEach( fixtures.data, (value, key) => dolza.store( key, value ) );
         _.forEach( fixtures.factories, (value, key) => dolza.register( key, value.factory, value.args ) );
@@ -24,5 +27,28 @@ describe( 'In production, Dolza', function() {
 
     it( 'stores and returns simple data', function () {
         expect( dolza.get( 'dbName' ) ).to.equal( 'smart' );
-    } )
+    } );
+
+    it( 'implements the singleton pattern for each key', function () {
+        const db1 = dolza.get( 'db' );
+        const db2 = dolza.get( 'db' );
+        expect( db1 === db2 ).to.be.true();
+        expect( db1 ).to.equal( db2 );
+    } );
+
+    it( 'instantiates a dependency graph as needed', function () {
+        const data = fixtures.data;
+        const expectedServiceId = `${data.dbServer}:${data.dbPort}/${data.dbName}$${data.salt}`;
+        const expectedRoutesId = `routes ${data.uuid} :: ${expectedServiceId}`;
+        const userRoutes = dolza.get( 'userRoutes' );
+        expect( userRoutes.getId() ).to.equal( expectedRoutesId );
+
+        const userService = dolza.get( 'userService' );
+        expect( userService.getId() ).to.equal( expectedServiceId );
+
+        const db1 = dolza.get( 'db' );
+        const db2 = dolza.get( 'db' );
+        expect( db1 === db2 ).to.be.true();
+        expect( db1 ).to.equal( db2 );
+    } );
 } );
